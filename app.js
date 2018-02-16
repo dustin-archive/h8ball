8fa5eceb5262ef49db9b92ce2c9a50d57f8f554a
Dustin Dowell <dustindowell22@gmail.com>
2018-02-16 01:38:18 -0600

    more answers

--- app.js
+++ app.js
@@ -5,20 +5,49 @@ const { floor, random } = Math
 
 const randomInt = max => floor(random() * floor(max))
 
+// # Sound Effects
+// + gta5 wasted
+// + boing
+// + price is right losing horn
+// +
+// +
+// +
+
+// # Images
+// +
+// +
+// +
+
 const state = {
-  result: 'yo playur, shake dat shiiit',
+  result: 'yo playur, shake dat shit',
   answers: [
     '404',
+    'did you agree to the TOS?',
     'do you hate your life?',
-    'nope sorry',
+    'LOL SIKE',
+    'never ask me that again',
+    'no one likes you',
+    'no, this is patrick',
+    'nope, sorry',
     'o boi',
     'siiigh',
+    'sorry, what?',
     'spicy bois ain\'t got chu',
     'the spicy bois have sealed yer fate',
+    'try again never',
+    'try me bitch',
     'WASTED',
+    'what\'s the point?',
+    'you don\'t deserve this',
+    'you would ask a dumb question',
+    'you wyld lmao',
     'YOU\'RE GARBAGE',
-    'no one likes you',
-    'sorry, what?'
+    'you\'re not good enough',
+    'you\'re p a t h e t i c in a e s t h e t i c',
+    'wordart, wordart everywhere',
+    '',
+    '',
+    '',
   ]
 }
 
@@ -44,16 +73,19 @@ const actions = {
 const view = (...args) => {
   const [ state, actions ] = args
 
+  const { isShaking, result } = state
+  const { shake } = actions
+
   return h('div', { class: 'app' }, [
     h('div', { class: 'app-box' }, [
       h('img', {
-        class: state.isShaking && 'shake',
+        class: isShaking && 'shake',
         src: 'favicon.png',
         onclick () {
-          actions.shake()
+          shake()
         }
       }),
-      h('h1', null, state.isShaking ? 'shaking dat...' : state.result)
+      h('h1', null, isShaking ? 'shaking dat shit...' : result)
     ])
   ])
 }
